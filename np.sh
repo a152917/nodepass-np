bcf1d62734bc9e5902f9ae716057c3f80787616b
饺子君 <momanluodi@gmail.com>
2026-01-19 14:18:34 +0800

    Update np.sh

--- np.sh
+++ np.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 set -e
 
-REPO_BASE="https://raw.githubusercontent.com/a152917/nodepass-np/main"
+BASE_URL="https://raw.githubusercontent.com/a152917/nodepass-np/main"
 TARGET_DIR="/etc/nodepass"
 
+# 创建目录
 mkdir -p "$TARGET_DIR"
 
-curl -fsSL "$REPO_BASE/np-dev" -o "$TARGET_DIR/np-dev"
-chmod +x "$TARGET_DIR/np-dev"
+# 下载文件到 /etc/nodepass
+curl -fsSL "$BASE_URL/np-dev" -o "$TARGET_DIR/np-dev"
+curl -fsSL "$BASE_URL/np-stb" -o "$TARGET_DIR/np-stb"
 
-curl -fsSL "$REPO_BASE/np-stb" -o "$TARGET_DIR/np-stb"
-chmod +x "$TARGET_DIR/np-stb"
+# 赋予执行权限
+chmod +x "$TARGET_DIR/np-dev" "$TARGET_DIR/np-stb"
 
-curl -fsSL "$REPO_BASE/np_local.sh" -o "./np_local.sh"
+# 下载 np_local.sh 到当前目录
+curl -fsSL "$BASE_URL/np_local.sh" -o "./np_local.sh"
 chmod +x "./np_local.sh"
