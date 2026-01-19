35596cb7ded46d201cbe4a013bcc2eac25f97dcd
饺子君 <momanluodi@gmail.com>
2026-01-19 14:11:00 +0800

    Update np.sh

--- np.sh
+++ np.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-
 set -e
 
 NODEPASS_DIR="/etc/nodepass"
+mkdir -p "$NODEPASS_DIR"
 
-if [ ! -d "$NODEPASS_DIR" ]; then
-    mkdir -p "$NODEPASS_DIR"
-fi
+curl -fsSL https://raw.githubusercontent.com/a152917/nodepass-np/main/np-dev -o "$NODEPASS_DIR/np-dev"
+curl -fsSL https://raw.githubusercontent.com/a152917/nodepass-np/main/np-stb -o "$NODEPASS_DIR/np-stb"
 
-curl -fsSL https://github.com/a152917/nodepass-np/blob/main/np-dev -o "$NODEPASS_DIR/np-dev"
-curl -fsSL https://github.com/a152917/nodepass-np/blob/main/np-stb -o "$NODEPASS_DIR/np-stb"
-
-curl -fsSL https://github.com/a152917/nodepass-np/blob/main/np_local.sh -o ./np_local.sh
+curl -fsSL https://raw.githubusercontent.com/a152917/nodepass-np/main/np_local.sh -o ./np_local.sh
 
 chmod +x ./np_local.sh
 ./np_local.sh
