9681387111f678fe1a0b8e76a3031d808949161f
饺子君 <momanluodi@gmail.com>
2026-01-19 14:06:22 +0800

    Update np.sh

--- np.sh
+++ np.sh
@@ -8,10 +8,10 @@ if [ ! -d "$NODEPASS_DIR" ]; then
     mkdir -p "$NODEPASS_DIR"
 fi
 
-curl -fsSL https://baidu.com/np-dev -o "$NODEPASS_DIR/np-dev"
-curl -fsSL https://baidu.com/np-stb -o "$NODEPASS_DIR/np-stb"
+curl -fsSL https://github.com/a152917/nodepass-np/blob/main/np-dev -o "$NODEPASS_DIR/np-dev"
+curl -fsSL https://github.com/a152917/nodepass-np/blob/main/np-stb -o "$NODEPASS_DIR/np-stb"
 
-curl -fsSL https://baidu.com/np_local.sh -o ./np_local.sh
+curl -fsSL https://github.com/a152917/nodepass-np/blob/main/np_local.sh -o ./np_local.sh
 
 chmod +x ./np_local.sh
 ./np_local.sh
